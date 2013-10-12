c2a9afee740e92bbc0ae4420d8c5248882d0b225
Christopher Canipe <christopher.canipe@pinnacol.com>
2013-10-12 01:46:23 -0600

    Add coveralls.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 gemspec
 
+gem 'coveralls', require: false
+
 group :test do
   gem 'fakefs', '~> 0.4.3', require: 'fakefs/safe'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ require 'rspec/autorun'
 require 'fakefs/spec_helpers'
 require 'rspec-html-matchers'
 
+require 'coveralls'
+Coveralls.wear!
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
